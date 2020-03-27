50800791a09734fc177b8725f4e90b4865fc0af3
xSuperMu <mohamed.whiteheart@yahoo.com>
2020-03-27 12:22:24 +0200

    solving a spoj problem

---
+++ SPOJ/spoj-SMPWOW.cpp
@@ -0,0 +1,17 @@
+// Problem ID:		SMPWOW
+// Problem Name:	SMPWOW - Wow
+// Problem Link:	https://www.spoj.com/problems/SMPWOW/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x;
+	cout << "W";
+	for (int i = 0; i < x; i++)
+		cout << "o";
+	cout << "w" << endl;
+
+	return 0;
+}
